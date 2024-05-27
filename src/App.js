a5bbd311e1a3a89c7f0ef59dc08531cf0f1b1d01
JayakrishnanPrakash <125813267+JayakrishnanPrakash@users.noreply.github.com>
2024-05-27 14:49:08 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          WELCOME TO EASY DEPLOY
+          WELCOME TO JENKINS DEPLOY
         </p>
         <a
           className="App-link"
